a4824709510b458cebaeb9d21445ca4b0e521fd1
akshaysthakare <thakareakshayofficial@gmail.com>
2024-12-11 16:33:08 +0530

    Created Login form Without Validation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,13 @@
 
 import './App.css'
-import { Button } from './components/ui/button'
+import Login from './auth/login'
 
 function App() {
-  
+
 
   return (
     <>
-      <Button className='bg-orange hover:bg-hoverOrange'> Lets build food app</Button>
+      <Login />
     </>
   )
 }
---
+++ client/src/auth/login.tsx
@@ -0,0 +1,45 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { Loader2, LockKeyhole, Mail } from "lucide-react";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  const loading = false;
+  return (
+    <div className="flex items-center justify-center min-h-screen w-screen">
+      <form className="md:p-8 w-full max-w-md md:border border-gray-200 rounded-lg mx-4 ">
+        <div className="mb-4">
+          <h1 className="font-bold text-2xl">Thakare Eats</h1>
+        </div>
+        <div className="mb-4">
+          <div className="relative">
+            <Input type="email" placeholder="Email" className="pl-10 focus-visible:ring-1" />
+            <Mail className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
+          </div>
+        </div>
+        <div className="mb-4">
+          <div className="relative">
+            <Input type="password" placeholder="Password" className="pl-10 focus-visible:ring-1" />
+            <LockKeyhole className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
+          </div>
+        </div>
+        <div className="mb-10">
+          {
+            loading ?
+              <Button className="w-full bg-orange hover:bg-hoverOrange">
+                <Loader2 className="mr-2 h-2 w-4 animate-spin" /> Please Wait</Button>
+              :
+              (<Button className="w-full bg-orange hover:bg-hoverOrange">Login</Button>)
+          }
+        </div>
+        <Separator />
+        <p>Don't have an account?{" "}
+          <Link to="/signup">Signup</Link>
+        </p>
+      </form>
+    </div>
+  )
+}
+
+export default Login;
