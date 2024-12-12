79e4f1e2461a89eefac19d0b88bef512f58175b5
akshaysthakare <thakareakshayofficial@gmail.com>
2024-12-12 16:48:37 +0530

    Added validation to Forget password using Zod

--- client/src/auth/ForgotPassword.tsx
+++ client/src/auth/ForgotPassword.tsx
@@ -1,15 +1,38 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input"
+import { ForgotPasswordInputState, userForgotPasswordSchema } from "@/schema/userSchema";
 import { Loader2, Mail } from "lucide-react"
-import { useState } from "react"
+import { ChangeEvent, FormEvent, useState } from "react"
 import { Link } from "react-router-dom";
 
 const ForgotPassword = () => {
-  const [email, setEmail] = useState<string>("");
+  const [input, setInput] = useState<ForgotPasswordInputState>({
+    email: ""
+  })
+
   const loading = false;
+
+
+  const [errors, setErrors] = useState<Partial<ForgotPasswordInputState>>({})
+  const changeEventHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setInput({ ...input, [name]: value })
+  }
+
+  const forgotPasswordSubmitHandler = (e: FormEvent) => {
+    e.preventDefault();
+    const result = userForgotPasswordSchema.safeParse(input)
+    if (!result.success) {
+      const fieldErrors = result.error.formErrors.fieldErrors;
+      setErrors(fieldErrors as Partial<ForgotPasswordInputState>)
+      return;
+    }
+    console.log(input)
+  }
+
   return (
     <div className="flex items-center justify-center min-h-screen w-full">
-      <form className="flex flex-col gap-5 md:p-8 w-full max-w-md rounded-lg mx-4">
+      <form onSubmit={forgotPasswordSubmitHandler} className="flex flex-col gap-5 md:p-8 w-full max-w-md rounded-lg mx-4">
         <div className="text-center">
           <h1 className="font-extrabold text-2xl mb-2">Forgot Password</h1>
           <p className="text-sm text-gray-600"> Enter Your Email Address to reset your password</p>
@@ -17,13 +40,17 @@ const ForgotPassword = () => {
         <div className="relative w-full">
           <Input
             type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            name="email"
+            value={input.email}
+            onChange={changeEventHandler}
             placeholder="Enter Your email"
             className="pl-10"
           />
           <Mail className="absolute inset-y-2 left-2 text-gray-600 pointer-events-none" />
         </div>
+        {
+          errors && <span className="text-sm text-red-500">{errors.email}</span>
+        }
         {
           loading ? (
             <Button disabled className="bg-orange hover:bg-hoverOrange"><Loader2 className="mr-2 h-4 w-4 animate-spin" /> Please Wait </Button>
@@ -31,7 +58,8 @@ const ForgotPassword = () => {
             <Button className="bg-orange hover:bg-hoverOrange"> Send Reset Link</Button>
           )
         }
-        <span>
+
+        <span className="text-center">
           Back to{" "}
           <Link to="/login" className="text-blue-500">Login</Link>
         </span>
--- client/src/schema/userSchema.tsx
+++ client/src/schema/userSchema.tsx
@@ -1,5 +1,5 @@
 
-import {z} from "zod"
+import { z } from "zod"
 
 export const userSignupSchema = z.object({
   fullname: z.string().min(1, "Fullname is required"),
@@ -13,7 +13,14 @@ export type SignupInputState = z.infer<typeof userSignupSchema>
 export const userLoginSchema = z.object({
   email: z.string().email("Invalid email address"),
   password: z.string().min(6, "Password must be atlest 6 character"),
- 
+
 })
 
 export type LoginInputState = z.infer<typeof userLoginSchema>
+
+
+export const userForgotPasswordSchema = z.object({
+  email: z.string().email("Invalid email address"),
+})
+
+export type ForgotPasswordInputState = z.infer<typeof userForgotPasswordSchema>
